8266d8101d0ac9cc80b66172a5b5b99dda5850f2
viditsingh17 <viditsinghbrahmania@gmail.com>
2021-04-24 11:07:52 +0530

    Server, script : add user name and time

--- script.js
+++ script.js
@@ -4,63 +4,93 @@ $('document').ready(()=>{
     var outputArea = $('#chat-output');
     var user_message = '';
     //function definitions here
-    function addBotMessage(message){
+    function getCurrentTime(){
+      const date = new Date($.now());
+      const hr = date.getHours();
+      const min = date.getMinutes();
+      var meridian = '';
+      if(hr>12){
+        hr = hr-12;
+        meridian = 'PM';
+      }
+      else{
+        meridian = 'AM';
+      }
+
+      const time = hr + ':' + min + ' ' + meridian;
+      return time;
+    }
+    function addBotMessage(message, name, time){
         outputArea.append(`
-          <div class='user-message'>
-            <div class='message'>
-              ${message}
-            </div>
+        <div class='user-message'>
+          <div class='message'>
+            <div class='name'>${name}</div>
+            ${message}
+            <div class='time'>${time}</div>
           </div>
+        </div>
         `);
       }
 
-    function addUserMessage(message){
-      const bot_message = $('<div>');
-      bot_message.addClass('bot-message').append(` <div class='message'>
-            ${message}
-          </div>`);
-      outputArea.append(bot_message);
-    
-      $('html, body').animate({
-        scrollTop: $('bot-message').offset.top + $('bot-message').scrollHeight
-      }, 2000);
+    function addUserMessage(message, name, time){
+      outputArea.append(`
+          <div class='bot-message'>
+            <div class='message'>
+              <div class='name'>${name}</div>
+              ${message}
+              <div class='time'>${time}</div>
+            </div>
+          </div>
+        `);
     }
 
     // Code starts here
     const socket = io('http://localhost:3000');
-    // const name = prompt('Name yourself?');
-    // socket.emit('new-user', name);
-    // outputArea.append(
-    //   `
-    //   <div class="user-connected">You joined</div>
-    //   `
-    // );
-    // socket.on('welcome-message', welcome_message=>{
-    //   outputArea.append(
-    //     `
-    //     <div class="welcome-message">${welcome_message}</div>
-    //     `
-    //   );
-    // });
-    // socket.on('user-connected', user=>{
-    //   outputArea.append(
-    //     `
-    //     <div class="user-connected">${user} joined</div>
-    //     `
-    //   );
-    // });
-    socket.on('chat-message', message=>{
-        console.log(message);
-        addBotMessage(message);
+    const name = prompt('Name yourself?');
+    socket.emit('new-user', name);
+    outputArea.append(
+      `
+      <div class="user-connected">You joined</div>
+      `
+    );
+    socket.on('welcome-message', welcome_message=>{
+      outputArea.append(
+        `
+        <div class="welcome-message">${welcome_message}</div>
+        `
+      );
+    });
+    socket.on('user-connected', user=>{
+      outputArea.append(
+        `
+        <div class="user-connected">${user} joined</div>
+        `
+      );
+    });
+    socket.on('user-disconnected', user=>{
+      outputArea.append(
+        `
+        <div class="user-disconnected">${user} left</div>
+        `
+      );
+    });
+    socket.on('chat-message', data=>{
+        addBotMessage(data.message, data.name, data.time);
     });
 
 
     $('#user-input-form').on("submit", function(e){
         user_message = $('#user-input').val();
+        const time = getCurrentTime();
         //If the string trims falsely that means it's either empty or contains only spaces
         if(user_message!=' '){
-            socket.emit('send-chat-message', user_message);
-            addUserMessage(user_message);
+            const data = {
+              message : user_message,
+              time : time,
+              name : name,
+            }
+            socket.emit('send-chat-message', data);
+            addUserMessage(user_message, 'You', time);
         }
         else{
             alert('Type something!');
--- server.js
+++ server.js
@@ -8,12 +8,17 @@ const users = {}
 
 //this function is called every time a user loades our website
 io.on('connection', socket=>{
-    socket.emit('welcome-message', 'Welcome, Your journey with Real Time Chat App starts here!');
-    // socket.on('new-user', name=>{
-    //     users[socket.id] = name;
-    //     socket.broadcast.emit('user-connected', name);
-    // });
-    socket.on('send-chat-message', message=>{
-        socket.broadcast.emit('chat-message',message);
+    socket.emit('welcome-message', 'Welcome, your journey with Real Time Chat App starts here!');
+    socket.on('new-user', name=>{
+        name==null?name='Barak Obama[Real]':name=name;
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
+    socket.on('send-chat-message', data=>{
+        socket.broadcast.emit('chat-message',data);
+    });
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
     });
 })
